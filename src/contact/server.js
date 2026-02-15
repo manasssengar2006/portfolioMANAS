630ba616207b16e3c801efc739990ee9a99d7de7
manasnotmanas <manasj261@gmail.com>
2026-02-15 08:25:51 +0530

    backend

--- src/contact/server.js
+++ src/contact/server.js
@@ -8,6 +8,9 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(cors({
+  origin: "*", // change later for security
+}));
 
 app.post("/send", async (req, res) => {
   const { name, email, message } = req.body;
@@ -40,4 +43,5 @@ app.post("/send", async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log("Server running on 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
